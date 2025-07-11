e1f913d2ad59f2d167a8cbc60a2ef221cf9bd57a
Douglas Trindade <douglastrindade22@hotmail.com>
2025-07-11 20:44:55 -0300

    wip: schudling

--- src/app/(protected)/agendamentos/domain/index.tsx
+++ src/app/(protected)/agendamentos/domain/index.tsx
@@ -2,10 +2,8 @@
 
 export const Appointments = () => {
   return (
-    <div className="h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        List
-      </div>
+    <div>
+      list
     </div>
   );
 };
